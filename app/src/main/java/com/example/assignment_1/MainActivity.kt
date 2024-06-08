b3fb955700de5ca61d0d1d46222d1629c0a0242e
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-06-08 19:38:58 +0500

    added alert box when no match found

--- app/src/main/java/com/example/assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.assignment_1
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,12 +21,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+//import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -34,6 +37,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
@@ -41,6 +45,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.rememberDialogState
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -190,6 +195,9 @@ fun MovieDetails(viewModel: MainViewModel) {
 fun MovieList(navController: NavController, results: List<Result>, viewModel: MainViewModel) {
 
     var searchText by remember { mutableStateOf("") }
+
+    var isDialogOpen by remember { mutableStateOf(false) }
+
     Log.i("TAG", "searchTextField: ${searchText}")
 
     //returns the filtered Movies on the basis of the search
@@ -199,27 +207,55 @@ fun MovieList(navController: NavController, results: List<Result>, viewModel: Ma
         results.filter { it.title.contains(searchText, ignoreCase = true) } // Filter movies by title
     }
 
-    Column (modifier = Modifier.padding(vertical = 20.dp, horizontal = 0.dp)) {
-        TextField(
-            value = searchText,
-            onValueChange = { searchText = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            placeholder = { Text("Search movies...") },
-            colors = TextFieldDefaults.textFieldColors(
-                focusedIndicatorColor = Color.Red,
-            )
+    LaunchedEffect(filteredMovies) {
+        isDialogOpen = filteredMovies.isEmpty()
+    }
+
+    if (isDialogOpen) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the alert dialog
+                isDialogOpen = false
+                searchText = ""
+            },
+            title = { Text("No Movies Found") },
+            text = { Text("There are no movies matching your search query.") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        // Dismiss the alert dialog
+                        isDialogOpen = false
+                        searchText = ""
+                    }
+                ) {
+                    Text("OK")
+                }
+            }
         )
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(150.dp),
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(filteredMovies) {
-                MovieCard(result = it, navController = navController, viewModel = viewModel)
+    }
+    else{
+        Column (modifier = Modifier.padding(vertical = 20.dp, horizontal = 0.dp)) {
+            TextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                placeholder = { Text("Search movies...") },
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Red,
+                )
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(150.dp),
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(filteredMovies) {
+                    MovieCard(result = it, navController = navController, viewModel = viewModel)
+                }
             }
         }
     }
